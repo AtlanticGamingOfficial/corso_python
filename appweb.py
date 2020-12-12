210c561d2395a24fef220aa80b1b4e8dba5794cf
Alessio Peternelli <alessio.peternelli@gmail.com>
2020-12-12 17:44:19 +0000

    extra details

--- appweb.py
+++ appweb.py
@@ -3,11 +3,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+def failasomma(a: int, b: int):
+    return a+b
+
 
 @app.route('/')
 def hello_world():
     a = 1
     b = 2
-    somma = a+b
+    somma = failasomma(a, b)
 
     return f'<span style="color: red;">{somma}</span>'
+
+@app.route('/hello/<x>')
+def hello_x(x):
+    return f'<span style="color: red;">Hello {x}</span>'
